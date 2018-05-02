d9acac48823c42e4daf735b1d4792f46af5857db
Louis Claridge <33028861+louisjclaridge@users.noreply.github.com>
2018-05-02 18:43:37 +0100

    added user, staff, customer and car classes. Also added createuser and login aspx pages

---
+++ Isad154_project/Classes/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Isad154_project.Classes
+{
+    public class User
+    {
+        public int accountID;
+        public string accountType;
+        public string firstName;
+        public string lastName;
+        public DateTime dateOfBirth;
+        public string address;
+        public string email;
+        public int phoneNumber;
+
+        public User(int userAccountID, string userAccountType, string userFirstName, string userLastName, DateTime userDateOfBirth, string userAddress, string userEmail, int userPhoneNumber)
+        {
+            accountID = userAccountID;
+            accountType = userAccountType;
+            firstName = userFirstName;
+            lastName = userLastName;
+            dateOfBirth = userDateOfBirth;
+            address = userAddress;
+            email = userEmail;
+            phoneNumber = userPhoneNumber;
+        }
+    }
+}
