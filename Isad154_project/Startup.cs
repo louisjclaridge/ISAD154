48ffb56bfd5b27c8486f359d53ab92a49969ab95
Louis Claridge <33028861+louisjclaridge@users.noreply.github.com>
2018-05-02 15:27:45 +0100

    changed master

---
+++ Isad154_project/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Isad154_project.Startup))]
+namespace Isad154_project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
