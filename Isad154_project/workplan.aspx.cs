180511d6aa06e3b8c63aa238165176947f5d234c
Johnathan Mawdsley <32461279+jjmawdsley@users.noreply.github.com>
2018-05-05 13:58:50 +0100

    cleaned up

--- Isad154_project/workplan.aspx.cs
+++ Isad154_project/workplan.aspx.cs
@@ -27,67 +27,14 @@ namespace Isad154_project
         {
             Workplan newWorkplan = new Workplan(txtProblem.Text, txtCheckInDate.Text, txtDueDate.Text);
             newWorkplan.CreateNewWorkplan();
-            //getWorkplanInputs();
-            //Workplan.CreateWorkplan(tempWorkplan);
 
 
 
-        }
-        Workplan tempWorkplan = null;
-        private void getWorkplanInputs()
-        {
-
-            
-
-            
-            string newWorkplanProblem = txtProblem.Text;
-            string newWorkplanCheckInDate = txtCheckInDate.Text;
-            string newWorkplanDueDate = txtDueDate.Text;
-            //ArrayList tempWorkplans = new ArrayList();
-
-            //var initialJson = File.ReadAllText("C:\\Users\\Work\\Documents\\GitHub\\ISAD154\\Isad154_project\\App_Data\\workplan.json");
-
-            //var array = JArray.Parse(initialJson);
-
-            //var itemToAdd = new JObject();
-            //itemToAdd["problem"] = newWorkplanProblem;
-            //itemToAdd["checkInDate"] = newWorkplanCheckInDate;
-            //itemToAdd["dueDate"] = newWorkplanDueDate;
-
-            //var jsonToOutput = JsonConvert.SerializeObject(array, Formatting.Indented);
-
-
-           
-            //Workplan newWorkplan = new Workplan(newWorkplanProblem, newWorkplanCheckInDate, newWorkplanDueDate);
-            //tempWorkplans.Add(tempWorkplan);
-
-            //MemoryStream stream1 = new MemoryStream();
-            //DataContract
-
-
-            //tempWorkplan = newWorkplan;
-
-            //return tempWorkplans;
-
-            //    StringBuilder sb = new StringBuilder();
-            //    StringWriter sw = new StringWriter(sb);
-
-            //    using (JsonWriter writer = new JsonTextWriter(sw))
-            //    {
-            //        writer.Formatting = Formatting.Indented;
-
-            //        writer.WriteStartObject();
-            //        writer.WritePropertyName("workplanProblem");
-            //        writer.WriteValue(newWorkplanProblem);
-            //        writer.WritePropertyName("workplanCheckInDate");
-            //        writer.WriteValue(newWorkplanCheckInDate);
-            //        writer.WritePropertyName("workplanDueDate");
-            //        writer.WriteValue(newWorkplanDueDate);
-            //    }
-            //    lblWorkplanNumber.Text = sw.ToString();
-            //}
-
-    
         }
     }
 }
+        
+        
+            
+
+            
